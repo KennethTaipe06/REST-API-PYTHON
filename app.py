a0b6489e5057ee7bfb852bde65eb62e7d08aa5f1
Kenneth Taipe <kennethtaipe@gmail.com>
2024-12-11 16:04:36 -0500

    feat: :fire: adding comments to better understanding

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 from flask import Flask
 from flasgger import Swagger
 
+# Crea una instancia de la aplicación Flask
 app = Flask(__name__)
+
+# Inicializa Swagger para la documentación automática de la API
 swagger = Swagger(app)
 
+# Define una ruta para el endpoint /api/hello con el método GET
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
     """
-    A simple hello world endpoint.
+    Un endpoint simple de hola mundo.
     ---
     responses:
       200:
-        description: A simple hello world message
+        description: Un mensaje simple de hola mundo
         content:
           text/html:
             schema:
               type: string
               example: "<h1>Hola Mundo</h1>"
     """
+    # Responde con un mensaje de hola mundo en HTML
     return "<h1>Hola Mundo</h1>"
 
+# Punto de entrada de la aplicación
 if __name__ == '__main__':
+    # Ejecuta la aplicación en el host 0.0.0.0 y el puerto 5000 en modo debug
     app.run(host='0.0.0.0', port=5000, debug=True)
